6467d1d12621995dc44baf40bccea42906394a68
Munir Mahmud <munirmahmud6@gmail.com>
2021-01-17 21:47:12 +0600

    Set user email and token to the store

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,39 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import Header from './components/Nav/Header';
+import { auth } from './config/firebase';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import RegisterComplete from './pages/Auth/RegisterComplete';
 import Home from './pages/Home';
 
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Check firebase auth state
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      console.log(user);
+      if(user) {
+        let idTokenResult = await user.getIdTokenResult();
+
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            name: user.email,
+            token: idTokenResult.token,
+          }
+        });        
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Header />
