a2b0bfcef1ac677117046a94d6d5e2cf5f7fc440
Munir Mahmud <munirmahmud6@gmail.com>
2021-01-17 17:48:33 +0600

    Registration form done

--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    };
+
     return (
-        <div>
-            Register
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 offset-3">
+                    <h3 className="text-center">Register</h3>
+
+                    <form onSubmit={handleSubmit}>
+                        <input type="email"
+                            className="form-control"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            autoFocus
+                        />
+
+                        <button type="submit" className="btn btn-raised">Register</button>
+                    </form>
+
+                    {/* {registerForm()} */}
+                </div>
+            </div>
         </div>
     );
 };
