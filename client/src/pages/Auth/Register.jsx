668757d16e156b8892ddfb281d4ce7a1125c11b6
Munir Mahmud <munirmahmud6@gmail.com>
2021-01-17 18:40:12 +0600

    Send email to the email for registration is done

--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import { auth } from '../../config/firebase';
 
 const Register = () => {
     const [email, setEmail] = useState("");
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(email);
+        const config = {
+            url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
+            handleCodeInApp: true,
+        }
+
+        await auth.sendSignInLinkToEmail(email, config);
+
+        // Show to success message to the user
+        toast.success(`An email is sent to ${email}. Please click the link to complete your registration.`);
+
+        // Save email in localstorage
+        window.localStorage.setItem('emailForRegistration', email);
+
+        // Remove the existing value
+        setEmail("");
+
     };
 
     return (
@@ -13,6 +31,8 @@ const Register = () => {
             <div className="row">
                 <div className="col-md-6 offset-3">
                     <h3 className="text-center">Register</h3>
+
+                    <ToastContainer />
 
                     <form onSubmit={handleSubmit}>
                         <input type="email"
@@ -24,8 +44,6 @@ const Register = () => {
 
                         <button type="submit" className="btn btn-raised">Register</button>
                     </form>
-
-                    {/* {registerForm()} */}
                 </div>
             </div>
         </div>
