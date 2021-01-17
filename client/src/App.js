2d9643efaefb87a8b3fb283e8a3d353a5e3e4a8b
Munir Mahmud <munirmahmud6@gmail.com>
2021-01-17 15:31:50 +0600

    Router setup for home, register & login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      New Project
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
   );
 }
 
