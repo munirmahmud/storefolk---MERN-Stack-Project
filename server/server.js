0be7a6c0f7c81d4c5b7dfcae0108bb7063e2712a
Munir Mahmud <munirmahmud6@gmail.com>
2021-01-18 11:29:33 +0600

    Database connection and middlewares setup done

---
+++ server/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@react-projects.8vnfa.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    
+// Database connection
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('DB connected'))
+.catch((err) => console.log("DB connection error: ", err.message));
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
+
+
+// Route
+app.get('/', (req, res) => {
+    res.send("Home Page")
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server connected on port ${process.env.PORT}: http://localhost:${process.env.PORT}`);
+})
+
