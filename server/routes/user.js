ad2d451896fbe7ad8c6b3212b70279471ceb7c3b
Munir Mahmud <munirmahmud6@gmail.com>
2021-01-18 11:43:55 +0600

    Routes autoloading done

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,9 +3,9 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get('/create-or-update-user', (req, res) => {
+router.get('/user', (req, res) => {
     res.json({
-        data: "Hello, this is new router"
+        data: "Hello, you hit user"
     });
 });
 
