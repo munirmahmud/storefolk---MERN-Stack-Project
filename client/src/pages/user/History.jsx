9eff9b6e49834801c0ee152fd2e12f5bb2c8f39d
Munir Mahmud <munirmahmud6@gmail.com>
2021-01-20 22:54:48 +0600

    Sidebar for logged in user done

--- client/src/pages/user/History.jsx
+++ client/src/pages/user/History.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import UserNav from '../../components/Nav/UserNav';
 
 const History = () => {
     return (
         <div className="container-fluid">
             <div className="row">
-                <div className="col">User History Page</div>
+                <div className="col-md-2">
+                    <h3>Menus</h3>
+                    <UserNav />
+                </div>
+
             </div>
         </div>
     )
