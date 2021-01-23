f836f7b948ed2a560947a41abc617c2e0a51ec6c
Munir Mahmud <munirmahmud6@gmail.com>
2021-01-23 18:50:18 +0600

    All the CRUD methods for category added

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -13,17 +13,37 @@ exports.create = async (req, res) => {
 };
 
 exports.list = async (req, res) => {
-
+    
+    const category = await Category.find({}).sort({createdAt: -1}).exec();
+    res.json(category);
 };
 
 exports.read = async (req, res) => {
-
+    const category = await Category.findOne({slug: req.params.slug}).exec();
+    res.json(category);
 };
 
 exports.update = async (req, res) => {
+    const { name } = req.body;
 
+    try {
+        const updated = await Category.findOneAndUpdate(
+            {slug: req.params.slug}, 
+            {name, slug: slugify(name)},
+            {new true}
+        );
+
+        res.json(updated);
+    } catch (error) {
+        res.status(400).send('Updating the category failed.');
+    }
 };
 
 exports.remove = async (req, res) => {
-
+    try {
+        const deletedCategory = await Category.findOneAndDelete({slug: req.params.slug});
+        res.json(deletedCategory);
+    } catch (error) {
+        res.json(400).send('Deleting category failed.');
+    }
 };
