f11632a791bade3bd3d1b97d4130fcae50912661
Munir Mahmud <munirmahmud6@gmail.com>
2021-01-23 12:28:41 +0600

    Admin authentication component done

---
+++ client/src/components/routes/AdminRoute.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Route } from 'react-router-dom';
+import { currentAdmin } from '../../helpers';
+import LoadingToRedirect from './LoadingToRedirect';
+
+const AdminRoute = ({children, ...rest}) => {
+    const { user } = useSelector(state => ({ ...state }));
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        if(user && user.token) {
+            currentAdmin(user.token)
+                .then((res) => {
+                    console.log('Current admin request ', res);
+                    setSuccess(true);
+                })
+                .catch((error) => {
+                    console.log('Admin error', error.message);
+                    setSuccess(false);
+                })
+        }
+    }, [user]);
+    
+    return (
+        <>
+            {success ? (
+                <Route 
+                    {...rest}
+                    render={() => children}
+                />
+            ) : (
+                <LoadingToRedirect />
+            )}
+        </>
+    )
+}
+
+export default AdminRoute;
