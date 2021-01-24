9c23d0e72db337f259dddf703ec94909930c150e
Munir Mahmud <munirmahmud6@gmail.com>
2021-01-24 12:28:46 +0600

    Displayed categories

--- client/src/pages/admin/category/CreateCategory.jsx
+++ client/src/pages/admin/category/CreateCategory.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import AdminNav from '../../../components/Nav/AdminNav';
-import { createCategory } from '../../../helpers/category';
+import { createCategory, getCategories } from '../../../helpers/category';
 
 const CreateCategory = () => {
     const { user } = useSelector((state) => ({...state}));
     const [name, setName] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        loadCategory();
+    }, [name]);
+
+    const loadCategory = () => {
+        getCategories().then((category) => {
+            setCategories(category.data);
+        })
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -55,6 +68,22 @@ const CreateCategory = () => {
                         </div>
                         <button type="submit" disabled={name.length < 3} className="btn btn-outline-primary">Save</button>
                     </form>
+
+                    <hr/>
+
+                    <ul className="nav flex-column">
+                    {categories.length > 0 && categories.map((category) => (
+                        <li className="alert alert-secondary d-flex justify-content-between" key={category._id}>
+                            <div>{category.name}</div>
+                            <div className="d-flex align-items-center">
+                                <DeleteOutlined className="mr-4 text-danger pointer" />
+                                <Link className="line-height-0" to={`/admin/category/${category.slug}`}>
+                                    <EditOutlined />
+                                </Link>
+                            </div>
+                        </li>
+                    ))}
+                    </ul>
                 </div>
 
             </div>
